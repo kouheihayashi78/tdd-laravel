9f5985ca29d8c29a2e3918a7bd364ef629853dd7
kouhei <kouhei.basketball7.8@gmail.com>
2025-03-18 15:18:03 +0900

    fix:データプロバイダを使ってリファクタリング

--- tests/Unit/Models/VacancyLevelTest.php
+++ tests/Unit/Models/VacancyLevelTest.php
@@ -8,34 +8,35 @@ use PHPUnit\Framework\TestCase;
 class VacancyLevelTest extends TestCase
 {
     /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    // public function test_example()
-    // {
-    //     $this->assertTrue(true);
-    // }
-
-    /**
-     * @test
      * @covers VacancyLevel::mark
-     *
-     * @return void
+     * @param int $remainingCount
+     * @param string $expectedMark
+     * @dataProvider dataMark
      */
-    public function testMark()
+    public function testMark(int $remainingCount, string $expectedMark): void
     {
-        // 0なら枠数なしとして×をテスト
-        $level = new VacancyLevel(0);
-        $this->assertSame('×', $level->mark());
+        $level = new VacancyLevel($remainingCount);
+        $this->assertSame($expectedMark, $level->mark());
+    }
 
-        // 1~4なら少し枠数があるとして△をテスト
-        $level = new VacancyLevel(4);
-        $this->assertSame('△', $level->mark());
-
-        // 5以上なら枠数が十分にあるとして◎をテスト
-        $level = new VacancyLevel(5);
-        $this->assertSame('◎', $level->mark());
-
+    // PHPUnitのデータプロバイダの仕組みを使ってテストパターンを配列で定義し、テストメソッドに順番に渡してテスト
+    public function dataMark()
+    {
+        return [
+            // 第一階層のキーはテストケースを表す(3パターン)
+            '空きなし' => [
+                // 連想配列のキーはテストメソッドの引数名と対応している(キーは書かなくても問題ないが明示的に書いた方がわかりやすい)
+                'remainingCount' => 0,
+                'expectedMark' => '×',
+            ],
+            '残りわずか' => [
+                'remainingCount' => 4,
+                'expectedMark' => '△',
+            ],
+            '空き十分' => [
+                'remainingCount' => 5,
+                'expectedMark' => '◎',
+            ]
+        ];
     }
 }
